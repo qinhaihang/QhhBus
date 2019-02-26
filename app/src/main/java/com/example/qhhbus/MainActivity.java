480229606d9a6a00d198636aca71bb14f140aba8
qinhaihang <hai449769034@qq.com>
2019-02-27 01:01:55 +0800

    1、第一次测试

--- app/src/main/java/com/example/qhhbus/MainActivity.java
+++ app/src/main/java/com/example/qhhbus/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.qhheventbus.QhhEventBusHelper;
+import com.example.qhheventbus.listener.IEventListener;
 
-    public Handler mHandler = new Handler(){
+public class MainActivity extends AppCompatActivity implements IEventListener {
+
+    public Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
@@ -19,12 +23,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        QhhEventBusHelper.getInstance().register(this);
         new Thread(new Runnable() {
             @Override
             public void run() {
                 Message message = new Message();
                 mHandler.sendMessage(message);
+                String msgStr = "message";
+                QhhEventBusHelper.getInstance().post(msgStr);
             }
         }).start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        QhhEventBusHelper.getInstance().unRegister(this);
+    }
+
+    @Override
+    public void onMessage(Object object) {
+        String msg = (String) object;
+        String threadName = Thread.currentThread().getName();
+        Log.d("qhh", "threadName = " + threadName + ", msg = " + msg);
     }
 }
